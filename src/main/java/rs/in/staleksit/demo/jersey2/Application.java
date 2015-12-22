ba774d03fc12d78cd47b9b4c2a84b41930bad7c1
Aleksandar Stoisavljevic <a.stoisavljevic@levi9.com>
2015-12-22 08:37:45 +0100

    changes to include eureka

--- src/main/java/rs/in/staleksit/demo/jersey2/Application.java
+++ src/main/java/rs/in/staleksit/demo/jersey2/Application.java
@@ -2,6 +2,7 @@ package rs.in.staleksit.demo.jersey2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @ImportResource(locations = {"classpath:org/jtransfo/spring/jTransfoContext.xml"})
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
